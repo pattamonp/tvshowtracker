1145cd6da644b3f643be9112cf5a854a0c113cc3
pattamonp <46978499+pattamonp@users.noreply.github.com>
2025-01-28 14:20:02 -0500

    Update tvshowtracker.py
    Removed index column and added more shows

--- tvshowtracker.py
+++ tvshowtracker.py
@@ -9,6 +9,7 @@ data = [
     {"Show": "Kitchen Nightmares‍🍳", "Platform": "Hulu", "Release Day": "Thursday"},
     {"Show": "Severance 💼", "Platform": "Apple", "Release Day": "Saturday"},
     {"Show": "Abbott Elementary 🎒", "Platform": "Hulu", "Release Day": "Thursday"},
+    {"Show": "Chef & My Fridge 👨‍🍳", "Platform": "Netflix", "Release Day": "Sunday"}
 ]
 
 df = pd.DataFrame(data)
@@ -17,15 +18,16 @@ today = datetime.now().strftime("%A")
 
 #UI
 st.title("TV Show Tracker")
-st.write("What's on TV today?")
+st.markdown("**What's on TV today?**")
 available_shows = df[df["Release Day"] == today]
 
 if not available_shows.empty:
     for _, row in available_shows.iterrows():
-        st.write(f"🎥 {row['Show']} on {row['Platform']}")
+        st.write(f"🎥 {row['Show']} on {row['Platform']}!")
 else:
-    st.write("No shows coming out today!")
+    st.write("Nothing to watch today. Sadness.")
 
 # Calendar View
 st.header("Full Release Schedule")
-st.table(df)
+#st.table(df)
+st.dataframe(df, hide_index=True, use_container_width=True)
